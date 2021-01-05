5f5018f736de55b1395d17618dd8d4de423ff0f0
William <work@suppo.fr>
2021-01-05 16:48:20 +0100

    Step 5.

--- index.php
+++ index.php
@@ -10,6 +10,7 @@ abstract class Animal
     public string $nom;
     protected DateTime $dateNaissance;
     private int $nbPattes;
+    public bool $bonneSante = true;
 
     public function __construct(string $nom, DateTime $dateNaissance, int $nbPattes)
     {
@@ -28,6 +29,11 @@ abstract class Animal
         $time = 15;
 
         return ( 10 * $this->nbPattes) / $time;
+    }
+
+    public function seBlesser(): void
+    {
+        $this->bonneSante = false;
     }
 
     abstract public function creerEnfant(): Animal;
@@ -85,6 +91,72 @@ class Elephant extends Animal
     }
 }
 
-$elephant = new Elephant('Maman Dumbo', new DateTime('2005-01-01'), 4);
+class Eleveur
+{
+    public string $nom;
 
-var_dump($elephant->creerEnfant());
+    protected array $animaux;
+
+    public function __construct(string $nom, array $animaux)
+    {
+        $this->nom = $nom;
+
+        foreach ($animaux as $animal) {
+            if (! $animal instanceof Animal) {
+                throw new Exception("L'éleveur s'occupe des animaux uniquement.");
+            }
+        }
+
+        if (count($animaux) < 1 || count($animaux) > 10) {
+            throw new Exception("Le nombre d'animaux doit être compris entre 1 et 10 inclus.");
+        }
+
+        $this->animaux = $animaux;
+    }
+
+    public function soigner(): int
+    {
+        $animauxBlesses = array_filter($this->animaux, function (Animal $animal) {
+            return $animal->bonneSante === false;
+        });
+
+        foreach ($animauxBlesses as $animal) {
+            $animal->bonneSante = true;
+        }
+
+        return count($animauxBlesses);
+    }
+}
+
+try {
+    $eleveur = new Eleveur('John', []);
+} catch (Exception $e) {
+    var_dump($e->getMessage());
+}
+
+try {
+    $eleveur = new Eleveur('Bob', [
+        new DateTime(),
+    ]);
+} catch (Exception $e) {
+    var_dump($e->getMessage());
+}
+
+$serpent = new Serpent('Maman Dumbo', new DateTime('2005-01-01'));
+$serpent->seBlesser();
+
+$elephant = new Elephant('Dumbo', new DateTime('2008-01-01'), 4);
+$elephant->seBlesser();
+
+$reptile = new Reptile('Croco', new DateTime('2010-01-01'), 4);
+
+$animaux = [
+    $serpent,
+    $elephant,
+    $reptile,
+];
+
+$eleveur = new Eleveur('Robert', $animaux);
+
+var_dump($eleveur->soigner());
+var_dump($eleveur->soigner());
